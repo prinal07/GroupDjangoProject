75c69731b946b27aa172790bd977ff913240abe1
prinal07 <96751895+prinal07@users.noreply.github.com>
2023-02-20 16:08:02 +0000

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'projectDB',
-        'USER': 'root',
-        'PASSWORD': 'Vinita01',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
