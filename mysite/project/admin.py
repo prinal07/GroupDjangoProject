95e212fe1b2ecc2b05685001eb1e9b3a8fb75d4b
ah1062 <95500509+ah1062@users.noreply.github.com>
2023-02-20 17:42:30 +0000

    Models, Start Database

--- mysite/project/admin.py
+++ mysite/project/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Group, Challenge
+
+admin.site.register(User)
+admin.site.register(Group)
+admin.site.register(Challenge)
+
--- mysite/project/models.py
+++ mysite/project/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    profileClass = models.IntegerField(default = 0)
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    email = models.CharField(max_length = 30)
+    username = models.CharField(max_length = 20)
+    password = models.CharField(max_length = 20)
+    group = models.IntegerField(default = 0)
+    level = models.IntegerField(default = 0)
+    points = models.IntegerField(default = 0)
+
+class Group(models.Model):
+    groupName = models.CharField(max_length = 30)
+    points = models.IntegerField(default = 0)
+    isDepartment = models.BooleanField()
+    isAccomodation = models.BooleanField()
+
+class Challenge(models.Model):
+    challengeText = models.CharField(max_length = 200)
