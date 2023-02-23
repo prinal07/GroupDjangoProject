200f14b3d7199d49c6e243b5c5f73e2eb1cc9b83
sc1131 <sc1131@exeter.ac.uk>
2023-02-23 12:19:03 +0000

    html login page and forms

--- mysite/project/views.py
+++ mysite/project/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
+from .forms import CreateUserForm
+
 # Create your views here.
 
 from django.http import HttpResponse
@@ -11,7 +13,14 @@ def index(request):
 
 
 def registerPage(request):
-    context = {}
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
     return render(request, 'project/register.html', context)
 
 
