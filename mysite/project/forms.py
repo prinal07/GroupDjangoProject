a646febb0aa735222047422f8c96b8c7ff938647
sc1131 <sc1131@exeter.ac.uk>
2023-02-23 10:57:02 +0000

    html registration page and forms

--- mysite/project/forms.py
+++ mysite/project/forms.py
@@ -3,17 +3,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class NewUserForm (UserCreationForm):
+class NewUserForm(UserCreationForm):
     emai = forms.EmailField(required=True)
+
 
 class Meta:
     model = User
-    fields = ("username","email","password1","password2")
+    fields = ("username", "email", "password1", "password2")
 
-def save(self,commit=True):
-    user = super(NewUserForm,self).save(commit=False)
-    user.email=self.claned_data['email']
+
+def save(self, commit=True):
+    user = super(NewUserForm, self).save(commit=False)
+    user.email = self.claned_data['email']
     if commit:
         user.save()
     return user
-
