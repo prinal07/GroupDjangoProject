df6258d06cf3c685088f9589abc4327db5b88aab
sc1131 <sc1131@exeter.ac.uk>
2023-02-23 11:20:52 +0000

    html registration page and forms

--- mysite/project/views.py
+++ mysite/project/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -9,6 +9,9 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'project/index.html')
 
-
+def registerPage(request):
+    form = UserCreationForm()
+    context = {}
+    return render(request, 'project/register.html',context)
 
 
