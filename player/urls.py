7d0dcb5dea172e12fb24dbc931fba91f774faa9a
sc1131 <sc1131@exeter.ac.uk>
2023-02-25 17:08:43 +0000

    Trying to fix profile picture feature. Limited progress (i.e. none)

--- player/urls.py
+++ player/urls.py
@@ -1,9 +1,12 @@
-from django.contrib import admin
-from django.urls import path,include
-from django.contrib.auth import views as auth_views
-from users import views as user_views
+from django.urls import path, include
 from player import views as player_views
+from django.conf import settings
+from django.conf.urls.static import static
 
+urlpatterns = [path('', player_views.home, name='overview'),
+               path('leaderboard/', player_views.leaderboard, name="leaderboard"),
+               path('profile/', player_views.profile, name='profile'),
+               ]
 
-urlpatterns = [path('', player_views.home, name="overview"),
-               path('leaderboard/',player_views.leaderboard,name="leaderboard")]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
