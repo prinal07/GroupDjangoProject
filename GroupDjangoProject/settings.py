a58ec56ad1b7df25dc944013ac00a0a053b6b4ef
sc1131 <sc1131@exeter.ac.uk>
2023-02-25 15:24:02 +0000

    Creating a profile page and adding profile to database

--- GroupDjangoProject/settings.py
+++ GroupDjangoProject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,3 +123,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'overview'
+
+
