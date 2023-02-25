11fc799d4d6aa222709fbf907e2aa781c82048bb
sc1131 <sc1131@exeter.ac.uk>
2023-02-25 18:23:56 +0000

    All of player app

---
+++ player/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from users.models import User
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'player/overview.html', {'title': 'Overview'})
+
+
+def leaderboard(request):
+    # leaderboard of everyone in given accommodation
+    accommodation = request.user.accommodation
+    all_users = User.objects.all().filter(accommodation=request.user.accommodation)
+    all_users = all_users.order_by('-points')
+
+    return render(request, 'player/leaderboard.html', {'title': 'Leaderboard', 'leaderboard': all_users}, )
+
+
+def profile(request):
+    return render(request, 'player/profile.html')
