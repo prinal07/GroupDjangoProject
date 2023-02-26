ebcddb798dcdeded5e457e9b49decc63b1e75ca7
sc1131 <sc1131@exeter.ac.uk>
2023-02-26 17:56:16 +0000

    Marginal improvements made to overview.html and leaderboard.html

--- player/views.py
+++ player/views.py
@@ -5,7 +5,22 @@ from django.db.models import Sum
 
 # Create your views here.
 def home(request):
-    return render(request, 'player/overview.html', {'title': 'Overview'})
+    # leaderboard of everyone in given accommodation
+    # matching the username of Django User class with username our user class
+    logged_username = request.user.username
+    logged_user = User.objects.get(username=logged_username)
+    print(logged_user.accommodation)
+
+    all_users_accommodation = User.objects.all().filter(accommodation=logged_user.accommodation)
+    all_users_accommodation = all_users_accommodation.order_by('-points')[:5]
+
+    # annotate creates new field for each accomdation group
+    # creating sum column for each accommodation
+    all_accommodations = User.objects.values('accommodation').annotate(Sum('points')).order_by()[:5]
+    print(all_accommodations)
+    return render(request, 'player/overview.html',
+                  {'title': 'Overview', 'user_acc_leaderboard': all_users_accommodation,
+                   'acc_leaderboard': all_accommodations})
 
 
 def leaderboard(request):
