556066a41ea886888bd5ddbf1a7464e636e9c3d0
sc1131 <sc1131@exeter.ac.uk>
2023-02-26 12:12:25 +0000

    Can print user accommodation and accommodation leaderboards.

--- player/views.py
+++ player/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from users.models import User
+from django.db.models import Sum
 
 
 # Create your views here.
@@ -13,10 +14,19 @@ def leaderboard(request):
     logged_username = request.user.username
     logged_user = User.objects.get(username=logged_username)
     print(logged_user.accommodation)
-    all_users = User.objects.all().filter(accommodation=logged_user.accommodation)
-    all_users = all_users.order_by('-points')
 
-    return render(request, 'player/leaderboard.html', {'title': 'Leaderboard', 'leaderboard': all_users}, )
+    all_users_accommodation = User.objects.all().filter(accommodation=logged_user.accommodation)
+    all_users_accommodation = all_users_accommodation.order_by('-points')
+
+    # annotate creates new field for each accomdation group
+    # creating sum column for each accommodation
+    all_accommodations = User.objects.values('accommodation').annotate(Sum('points')).order_by()
+    print(all_accommodations)
+
+    return render(request, 'player/leaderboard.html',
+                  {'title': 'Leaderboard', 'user_acc_leaderboard': all_users_accommodation,
+                   'acc_leaderboard': all_accommodations}
+                  )
 
 
 def profile(request):
