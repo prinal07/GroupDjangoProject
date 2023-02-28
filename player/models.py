2bdfe7dd7410f74f21401a808e4ac87711ec2c92
sc1131 <sc1131@exeter.ac.uk>
2023-02-28 09:41:46 +0000

    Added Fact model

--- player/models.py
+++ player/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class FactofDay(models.Model):
+    date = models.DateField()
+    fact = models.TextField()
+
+    def __str__(self):
+        return f"{self.date} - {self.fact}"
