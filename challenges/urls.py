50cc8472e842d79af6ab7809a2fc48b8d90281ba
prinal07 <96751895+prinal07@users.noreply.github.com>
2023-03-01 17:51:20 +0000

    QR with Map with Challenges

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,8 +2,7 @@ from django.urls import path, include
 
 from challenges import views as challenge_views
 
-urlpatterns = [path('', challenge_views.challengesOverview, name='challenge_overview'),
-               path('collectibles/', challenge_views.collectibles, name="collectibles"),
-               path('challengeManager/', challenge_views.challengeManager, name='challengeManager'),
+urlpatterns = [path('collectibles/', challenge_views.collectibles, name="collectibles"),
+               path('', challenge_views.challengeManager, name='challengeManager'),
                path('QR/', challenge_views.QR, name='QR'),
                ]
