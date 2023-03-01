0e9d687a0f600c3fb677bd3240fa379c1b4a09ab
prinal07 <96751895+prinal07@users.noreply.github.com>
2023-03-01 16:01:41 +0000

    Try QR

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 from challenges import views as challenge_views
 
 urlpatterns = [path('', challenge_views.challengesOverview, name='challenge_overview'),
-               path('collectibles/', challenge_views.collectibles, name="collectibles")
+               path('collectibles/', challenge_views.collectibles, name="collectibles"),
+               path('challengeManager/', challenge_views.challengeManager, name='challengeManager'),
+               path('QR/', challenge_views.QR, name='QR'),
                ]
