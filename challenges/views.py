f29fdd67eaaae497abcb5999d6d3cbb0b90b8a6d
prinal07 <96751895+prinal07@users.noreply.github.com>
2023-03-01 20:27:07 +0000

    changes

--- challenges/views.py
+++ challenges/views.py
@@ -18,3 +18,16 @@ def collectibles(request):
 
 def QR(request):
     return render(request, 'challenges/QR.html')
+
+# views.py
+
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def scan_again(request):
+    account = request.account
+    points =  account.points
+    points = points + 10
+    account.save()
+    return HttpResponse('Counter incremented!')
