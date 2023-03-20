3a8f938d57df1a5266b39722a5d91bfd3e65cf2a
prinal07 <96751895+prinal07@users.noreply.github.com>
2023-03-20 19:36:57 +0000

    Update settings.py

--- GroupDjangoProject/settings.py
+++ GroupDjangoProject/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 # Add static folder to STATIC_DIRS
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 django_heroku.settings(locals())
