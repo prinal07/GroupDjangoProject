e3dc68c1db48b598a52dfaf70ce490927cc0baa7
prinal07 <96751895+prinal07@users.noreply.github.com>
2023-03-20 20:08:54 +0000

    Update settings.py

--- GroupDjangoProject/settings.py
+++ GroupDjangoProject/settings.py
@@ -137,12 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'static/'
-STATIC_ROOT =os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # Add static folder to STATIC_DIRS
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
-]
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 django_heroku.settings(locals())
 # Stores uploaded profile images
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
