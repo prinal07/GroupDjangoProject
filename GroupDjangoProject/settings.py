0ba3a20eb77b3c741eb3c39a5d7b3d65b2810c4a
prinal07 <96751895+prinal07@users.noreply.github.com>
2023-03-20 19:40:21 +0000

    Update settings.py

--- GroupDjangoProject/settings.py
+++ GroupDjangoProject/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'static/'
-STATIC_ROOT =os.path.join(BASE_DIR, 'static')
+STATIC_ROOT =os.path.join(BASE_DIR, 'static/')
 # Add static folder to STATIC_DIRS
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
