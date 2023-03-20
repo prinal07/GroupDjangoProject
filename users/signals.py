730d338eb97404bc6a56c97a945c18f0015a3bde
sc1131 <sc1131@exeter.ac.uk>
2023-03-20 13:20:25 +0000

    comments added to signals.py

--- users/signals.py
+++ users/signals.py
@@ -7,16 +7,27 @@ from .models import Profile, Account, Challenge, ChallengeTracker
 
 @receiver(post_save, sender=django_user)
 def create_profile(sender, instance, created, **kwargs):
+    """
+    Creates a profile picture based on the Django user
+    """
     if created:
         Profile.objects.create(user=instance)
 
 
 @receiver(post_save, sender=django_user)
 def save_profile(sender, instance, **kwargs):
+    """
+    Ensures profile is saved in database
+    """
     instance.profile.save()
+
 
 @receiver(post_save, sender=Challenge)
 def create_challengestatus(sender, instance, created, **kwargs):
+    """
+    Creates a challenge under account model
+
+    """
     if not created:
         # Challenge object has been updated
         for status in ChallengeTracker.objects.filter(challenge=instance):
@@ -26,10 +37,13 @@ def create_challengestatus(sender, instance, created, **kwargs):
                 challenge=instance,
                 completed=status.completed
             )
-            
+
 
 @receiver(post_save, sender=Account)
 def create_account_challenges(sender, instance, created, **kwargs):
+    """
+
+    """
     if created:
         challenges = Challenge.objects.all()
         for challenge in challenges:
