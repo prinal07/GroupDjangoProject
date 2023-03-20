30323145225f6a2f95fcb54f4bb6253729751511
prinal07 <96751895+prinal07@users.noreply.github.com>
2023-03-20 18:53:04 +0000

    Trials With Procfile

--- GroupDjangoProject/settings.py
+++ GroupDjangoProject/settings.py
@@ -139,7 +139,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'static/'
 STATIC_ROOT =os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 django_heroku.settings(locals())
 # Stores uploaded profile images
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
