eb72ee2ab8b50da2a3003ee6a6cb7e6f5647d5ac
prinal07 <96751895+prinal07@users.noreply.github.com>
2023-03-23 10:27:35 +0000

    Update unity.js

--- game/static/js/unity.js
+++ game/static/js/unity.js
@@ -3,7 +3,7 @@ function checkState() {
   if (status.innerHTML == "CORRECT") {
       $.ajax({
     type: "POST",
-    url: "game/unity/",
+    url: "game/unity",
     data: {
         "give_points": "true"
     },
@@ -33,7 +33,7 @@ function checkState() {
   else if (status.innerHTML == "INCORRECT") {
     $.ajax({
         type: "POST",
-        url: "game/unity/",
+        url: "game/unity",
         data: {
       "give_points": "false"
         },
